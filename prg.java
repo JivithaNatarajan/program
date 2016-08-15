34305b0d2d2579eb52f452208b528546d791b601
JivithaNatarajan <jivithasn@gmail.com>
2016-08-15 15:42:46 +0530

    Create prg.java

---
+++ prg.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+Class prime
+  {
+      public static void main(String args[])
+        {
+            int n=20;
+             if(n%2==0)
+             {
+               System.out.println("The number is even");
+             }
+            else
+             {
+              System.out.println("The number is odd");
+              }
+          }
+  }
